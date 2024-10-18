d4e63853963ad4e34c46e1b8f4ef5838d5749958
xlocaste <irfandi.di122@gmail.com>
2024-10-18 11:14:41 +0700

    start project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        customGray: '#2f2e2e',
+      },
+    },
+  },
+  plugins: [],
+}
